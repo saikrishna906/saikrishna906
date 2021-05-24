98c302d638520bb02bc81d75c8730526742a650c
saikrishna906 <kitturebal19@gmail.com>
2021-05-24 11:01:17 +0530

    day2

---
+++ day2/src/main/java/classday2/variable.java
@@ -0,0 +1,19 @@
+package classday2;
+
+public class variable {
+	static byte i;
+	static short j;
+	static int m;
+	long n ; float o; double p;char q;boolean l;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(i +"\n"+j+"\n"+m);
+		variable v=new variable();
+		System.out.println(v.n);
+		System.out.println(v.o);System.out.println(v.p);System.out.println(v.q);System.out.println(v.l);
+
+	}
+
+}
